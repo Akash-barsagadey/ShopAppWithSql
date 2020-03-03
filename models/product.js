5d940f99cebe0df06f21df738003011af5d3e369
Akash Barsagadey <akash_udaram.barsagadey@syngenta.com>
2020-03-03 09:16:38 +0530

    Added sequelize and create model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,4 @@
-const db = require('../util/database');
+/* const db = require('../util/database');
 
 const Cart = require('./cart');
 
@@ -27,4 +27,32 @@ module.exports = class Product {
   static findById(id) {
     return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
   }
-};
+}; */
+
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = Product;
